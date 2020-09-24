585336fc5b8d6c304ce204c0b030cd41e63a6fbd
Michael Misch <michealmisch1985@gmail.com>
2020-09-24 16:45:46 -0700

    Remove these properly

--- ctl.c
+++
@@ -1,5 +0,0 @@
-#include <u.h>
-#include <libc.h>
-#include "fs.h"
-
-/* A ctl file will call /usr/local/lib/unixsrv/bind, mount, etc */
--- notes.c
+++
@@ -1,5 +0,0 @@
-#import <u.h>
-#import <libc.h>
-#import "fs.h"
-
-/* Notes are in-memory, per connection */
